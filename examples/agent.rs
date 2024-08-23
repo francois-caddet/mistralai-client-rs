e685e9c1ac4de796d01125a7f812dce4071feb01
François Caddet <francois.caddet@icloud.com>
2024-08-23 12:20:19 +0200

    feat(Agent): Add unstreamed agents completions methods.
    - Add async and blocking agent completions methods into `Client`
    - Implement new types into a new `agent` module.
    - Add a trait to get choices from responses beeing generic over the response type
    - impl this trait for `ChatResponse` and `AgentResponse`
    - Add a blocking example for agent completion.

---
+++ examples/agent.rs
@@ -0,0 +1,24 @@
+use mistralai_client::v1::{
+    agent::{AgentMessage, AgentMessageRole, AgentParams},
+    client::Client,
+};
+
+fn main() {
+    // This example suppose you have set the `MISTRAL_API_KEY` environment variable.
+    let client = Client::new(None, None, None, None).unwrap();
+
+    let agid = std::env::var("MISTRAL_API_AGENT").expect("Please export MISTRAL_API_AGENT with the agent id you want to use");
+    let messages = vec![AgentMessage {
+        role: AgentMessageRole::User,
+        content: "Just guess the next word: \"Eiffel ...\"?".to_string(),
+        tool_calls: None,
+    }];
+    let options = AgentParams {
+        random_seed: Some(42),
+        ..Default::default()
+    };
+
+    let result = client.agent(agid, messages, Some(options)).unwrap();
+    println!("Assistant: {}", result.choices[0].message.content);
+    // => "Assistant: Tower. The Eiffel Tower is a famous landmark in Paris, France."
+}
--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -1,4 +1,5 @@
 pub mod chat;
+pub mod agent;
 pub mod chat_stream;
 pub mod client;
 pub mod common;
