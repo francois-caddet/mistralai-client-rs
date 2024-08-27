161b33c72539a6e982207349942a436df95399b7
francois-caddet <francois.caddet@icloud.com>
2024-08-27 09:25:59 +0200

    feat(constants): update model constants (#17)
    * feat(Model)!: Update model constants
    
    - Add new models Mistral Nemo and Codestral Mamba
    - Add aliases to models constant to be deserialized seemlessly from the versioned forme (e.g.: "mistral-large-2407")
    - The commonly named Mistral Large 2 is now `constants::Model::MistralLargeLatest` when the old one is `Model::MistralLarge`
    
    * tests(Model): Update the tests to check the new model variants.

--- src/v1/constants.rs
+++ src/v1/constants.rs
@@ -10,16 +10,22 @@ pub enum Model {
     OpenMixtral8x7b,
     #[serde(rename = "open-mixtral-8x22b")]
     OpenMixtral8x22b,
+    #[serde(rename = "open-mistral-nemo", alias = "open-mistral-nemo-2407")]
+    OpenMistralNemo,
     #[serde(rename = "mistral-tiny")]
     MistralTiny,
-    #[serde(rename = "mistral-small-latest")]
+    #[serde(rename = "mistral-small-latest", alias = "mistral-small-2402")]
     MistralSmallLatest,
-    #[serde(rename = "mistral-medium-latest")]
+    #[serde(rename = "mistral-medium-latest", alias = "mistral-medium-2312")]
     MistralMediumLatest,
-    #[serde(rename = "mistral-large-latest")]
+    #[serde(rename = "mistral-large-latest", alias = "mistral-large-2407")]
     MistralLargeLatest,
-    #[serde(rename = "codestral-latest")]
+    #[serde(rename = "mistral-large-2402")]
+    MistralLarge,
+    #[serde(rename = "codestral-latest", alias = "codestral-2405")]
     CodestralLatest,
+    #[serde(rename = "open-codestral-mamba")]
+    CodestralMamba,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
