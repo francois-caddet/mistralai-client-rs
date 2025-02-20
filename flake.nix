91df3029add805350b57e1d703444eaafafa83ac
François Caddet <francois.caddet@icloud.com>
2025-02-20 17:11:07 +0100

    Make the flake more simple to at least have a devshell working with recent rust version

--- flake.nix
+++ flake.nix
@@ -2,31 +2,42 @@
   description = "My flake with dream2nix packages";
 
   inputs = {
-    dream2nix.url = "github:nix-community/dream2nix";
-    nixpkgs.follows = "dream2nix/nixpkgs";
+rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     self,
-    dream2nix,
+    rust-overlay,
     nixpkgs,
     ...
   }: let
     system = "x86_64-linux";
+      pkgs = import nixpkgs {
+          inherit system;
+        overlays = [ (import rust-overlay) ];
+        };
   in {
-    # All packages defined in ./packages/<name> are automatically added to the flake outputs
-    # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
-    packages.${system}.default = dream2nix.lib.evalModules {
-      packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
-      modules = [
-        ./default.nix
-        {
-          paths.projectRoot = ./.;
-          # can be changed to ".git" or "flake.nix" to get rid of .project-root
-          paths.projectRootFile = "flake.nix";
-          paths.package = ./.;
-        }
-      ];
-    };
+        devShells.${system}.default = pkgs.mkShell {
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+        "PKG_CONFIG_PATH" = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+          # Extra inputs
+          buildInputs = [
+          ];
+
+          # Extra inputs can be added here
+          nativeBuildInputs = with pkgs; [
+            cargo
+            cargo-edit
+            rustfmt
+            rustc
+            nixpkgs-fmt
+            pkg-config
+          ];
+        };
   };
 }
